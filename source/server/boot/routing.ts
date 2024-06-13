9266c7f59d695bb3106eba30f3c4440ebec994e1
Martin Erlandsson <martin.erlandsson@morethandevs.com>
2024-06-13 20:44:26 +0200

    Correcting routing, making it simple

--- source/server/boot/routing.ts
+++ source/server/boot/routing.ts
@@ -9,13 +9,11 @@ import martinRoutes from "../routes/martinRoutes.js"
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-export function subDomain(checkDomain: string, fn: (req: express.Request, res: express.Response, next: express.NextFunction) => void): any {
+export function routeDomain(checkDomain: string, fn: (req: express.Request, res: express.Response, next: express.NextFunction) => void): any {
     return function (req: express.Request, res: express.Response, next: express.NextFunction) {
 
-        //Check if provided domain is equal to the incoming
-        let sub = ""
-        if (req.hostname.indexOf(".") > -1) { sub = req.hostname.substring(0, req.hostname.indexOf(".")) } //Perhaps do this once in a prior middleware
-        if (sub.toLowerCase() == checkDomain.toLowerCase()) { return fn(req, res, next) }
+        //Doing this simple, just routing the first domain
+        if (req.hostname.split(".")[0]?.toLowerCase() == checkDomain.toLowerCase()) { return fn(req, res, next) }
 
         //No match, continue in the request process
         next()
@@ -25,16 +23,16 @@ export function subDomain(checkDomain: string, fn: (req: express.Request, res: e
 
 
 export function routeMore(server: express.Application) {
-    server.use(subDomain('', express.static(path.join(__dirname, '..', '../moreSPA/')))) //Static for all the assets
-    server.use(subDomain('', moreRoutes)) //Route for public Index
+    server.use(routeDomain('morethandevs', express.static(path.join(__dirname, '..', '../moreSPA/')))) //Static for all the assets
+    server.use(routeDomain('morethandevs', moreRoutes)) //Route for public Index
 }
 
 export function routeMartin(server: express.Application) {
-    server.use(subDomain('martin', express.static(path.join(__dirname, '..', '../martinSPA/')))) //Static for all the assets
-    server.use(subDomain('martin', martinRoutes)) //Route for public Index
+    server.use(routeDomain('martinerlandsson', express.static(path.join(__dirname, '..', '../martinSPA/')))) //Static for all the assets
+    server.use(routeDomain('martinerlandsson', martinRoutes)) //Route for public Index
 }
 
 export function routeBirdy(server: express.Application) {
-    server.use(subDomain('bird', express.static(path.join(__dirname, '..', '../birdySPA/')))) //Static for all the assets
-    server.use(subDomain('bird', birdyRoutes)) //Route for public Index
+    server.use(routeDomain('bird', express.static(path.join(__dirname, '..', '../birdySPA/')))) //Static for all the assets
+    server.use(routeDomain('bird', birdyRoutes)) //Route for public Index
 }
